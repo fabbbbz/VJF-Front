468c6a69b20756c74e19d8946be4aef73fc99a76
Marion Saul <marionsaul.dev@gmail.com>
2021-10-26 11:52:06 +0200

    handle allergies in store and db

--- App.js
+++ App.js
@@ -6,11 +6,12 @@ import { createStackNavigator } from '@react-navigation/stack'
 import diet from './reducers/diet' /*importe la fonction exportée par notre reduceur*/
 import token from './reducers/token' /*import token from reducer*/
 import donts from './reducers/donts'
+import allergies from './reducers/allergies'
 import { Provider } from 'react-redux'
 
 import { createStore, combineReducers } from 'redux'
 
-const store = createStore(combineReducers({ diet, token, donts }))
+const store = createStore(combineReducers({ diet, token, donts, allergies }))
 
 import FirstScreen from './Screens/FirstScreen'
 import Home from './Screens/Home'
