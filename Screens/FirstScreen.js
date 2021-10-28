c8d2c3a45a0635f5cbb36362a4442f42be38aefd
Clementine <klmsenda@hotmail.com>
2021-10-28 09:51:00 +0200

    fichiers manquants

--- Screens/FirstScreen.js
+++ Screens/FirstScreen.js
@@ -8,16 +8,6 @@ import { useState, useEffect } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 function FirstScreen(props) {
-<<<<<<< HEAD
-	// If token in local storage redirect to mood 
-	// useEffect(() => {
-	//     AsyncStorage.getItem('token', (error, value) => {
-	//         if (value) {
-	//             props.navigation.navigate('Mood', { screen: 'Mood' })
-	//         }
-	//     });
-	// }, []);
-=======
 	useEffect(() => {
 		AsyncStorage.getItem('token', (error, value) => {
 			if (value) {
@@ -25,7 +15,6 @@ function FirstScreen(props) {
 			}
 		})
 	}, [])
->>>>>>> master
 
 	return (
 		<View style={styles.container}>
@@ -33,35 +22,15 @@ function FirstScreen(props) {
 				source={require('../assets/VJF-logo.png')}
 				style={{ width: 200, height: 200, marginTop: 100 }}
 			/>
-<<<<<<< HEAD
-			<Text h1 style={{ textAlign: 'center', color: '#000000', marginTop: 50 }}>
-				Vite j'ai faim!
-			</Text>
-
-=======
 
 			<Text h1 style={{ textAlign: 'center', color: '#000000', marginTop: 50 }}>
 				Vite j'ai faim!
 			</Text>
 
->>>>>>> master
 			<Text h4 style={{ textAlign: 'center', color: '#F2A902', marginTop: 50 }}>
 				Vous nous renseignez, on choisit pour vous!
 			</Text>
 			<ScrollView>
-<<<<<<< HEAD
-				<NextButton title="HOME"
-					onPress={() => { props.navigation.navigate('Home', { screen: 'Home' }) }} />
-				<NextButton title="SIGNUP"
-					onPress={() => { props.navigation.navigate('SignUp', { screen: 'SignUp' }) }} />
-				<NextButton title="MOOD"
-					onPress={() => { props.navigation.navigate('Mood', { screen: 'Mood' }) }} />
-				<NextButton title="HISTORY"
-					onPress={() => { props.navigation.navigate('History', { screen: 'History' }) }} />
-			</ScrollView>
-		</View>
-	);
-=======
 				<NextButton
 					title="HOME"
 					onPress={() => {
@@ -96,10 +65,17 @@ function FirstScreen(props) {
 						})
 					}}
 				/>
+				<NextButton
+					title="HISTORY"
+					onPress={() => {
+						props.navigation.navigate('History', {
+							screen: 'History',
+						})
+					}}
+				/>
 			</ScrollView>
 		</View>
 	)
->>>>>>> master
 }
 
 const styles = StyleSheet.create({
@@ -108,23 +84,9 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		backgroundColor: '#F4F4F4',
 	},
-<<<<<<< HEAD
-});
-
-//get token from store 
-function mapStateToProps(state) {
-	return { token: state.token }
-}
-
-export default connect(
-	mapStateToProps,
-	null
-)(FirstScreen)
-=======
 })
 //get token from store
 function mapStateToProps(state) {
 	return { token: state.token }
 }
 export default connect(mapStateToProps, null)(FirstScreen)
->>>>>>> master
