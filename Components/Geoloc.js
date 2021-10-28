fb29d5c00fdfa8de2f9e545e468104870fa95e0a
Marion Saul <marionsaul.dev@gmail.com>
2021-10-28 10:31:53 +0200

    suprr consolelogs invasifs

--- Components/Geoloc.js
+++ Components/Geoloc.js
@@ -1,39 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import { Text } from 'react-native'
 
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location'
+import * as Permissions from 'expo-permissions'
 
 export default function Geoloc() {
+	const [displayCurrentAddress, setDisplayCurrentAddress] = useState('')
 
-    const [displayCurrentAddress, setDisplayCurrentAddress] = useState('')
+	useEffect(() => {
+		async function askPermissions() {
+			var { status } = await Permissions.askAsync(Permissions.LOCATION)
+			if (status === 'granted') {
+				let { coords } = await Location.getCurrentPositionAsync()
+				if (coords) {
+					const { latitude, longitude } = coords
+					let response = await Location.reverseGeocodeAsync({
+						latitude,
+						longitude,
+					})
+					for (let item of response) {
+						let address = `${item.name}, ${item.street}, ${item.postalCode}, ${item.city}`
+						setDisplayCurrentAddress(address)
+					}
+				}
+			}
+		}
+		askPermissions()
+	}, [])
 
-    useEffect(() => {
-        async function askPermissions() {
-            var { status } = await Permissions.askAsync(Permissions.LOCATION);
-            if (status === 'granted') {
-                let { coords } = await Location.getCurrentPositionAsync();
-                if (coords) {
-                    const { latitude, longitude } = coords;
-                    let response = await Location.reverseGeocodeAsync({
-                        latitude,
-                        longitude
-                    });
-                    for (let item of response) {
-                        let address = `${item.name}, ${item.street}, ${item.postalCode}, ${item.city}`;
-                        setDisplayCurrentAddress(address);
-                    }
-                }
-                console.log('coords', coords)
-                console.log('address', displayCurrentAddress)
-            }
-        }
-        askPermissions();
-    }, []);
-
-
-
-    return (
-        <Text >{displayCurrentAddress}</Text>
-    )
+	return <Text>{displayCurrentAddress}</Text>
 }
