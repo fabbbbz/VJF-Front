b53d312621dd916b918fd61714f52d08b94b99c5
Marion Saul <marionsaul.dev@gmail.com>
2021-10-29 15:32:06 +0200

    fix buttons look

--- Components/NextButton.js
+++ Components/NextButton.js
@@ -16,7 +16,7 @@ const NextButton = ({ title, onPress }) => {
 				style={{
 					display: 'flex',
 					flexDirection: 'row',
-					justifyContent: 'space-between',
+					justifyContent: 'center',
 				}}
 				onPress={onPress}
 			>
@@ -43,7 +43,8 @@ const styles = StyleSheet.create({
 		color: '#fff',
 		fontSize: 16,
 		fontWeight: '700',
-		marginLeft: 65,
+		flex: 1,
+		textAlign: 'center',
 	},
 })
 
