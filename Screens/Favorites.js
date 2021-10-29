247fbc6638f293eff5e18d993206f2eb637cf46e
Marion Saul <marionsaul.dev@gmail.com>
2021-10-29 16:31:20 +0200

    fix css favorites iphone

--- Screens/Favorites.js
+++ Screens/Favorites.js
@@ -7,93 +7,91 @@ import { Ionicons } from '@expo/vector-icons'
 import { MY_IP } from '@env'
 
 function Favorites(props) {
-    const [favData, setFavData] = useState([])
-    useEffect(() => {
-        async function loadFavorites() {
-            const token = props.token
-            var rawResponse = await fetch(
-                `https://vitejaifaim-master-i57witqbae0.herokuapp.com/users/favorites/${token}`
-            )
-            var response = await rawResponse.json()
+	const [favData, setFavData] = useState([])
+	useEffect(() => {
+		async function loadFavorites() {
+			const token = props.token
+			var rawResponse = await fetch(
+				`https://vitejaifaim-master-i57witqbae0.herokuapp.com/users/favorites/${token}`
+			)
+			var response = await rawResponse.json()
 
-            setFavData(response.favorites)
-        }
+			setFavData(response.favorites)
+		}
 
-        loadFavorites()
-    }, [])
+		loadFavorites()
+	}, [])
 
+	var favList = favData.map((fav, i) => {
+		return (
+			<Card
+				key={i}
+				containerStyle={{
+					borderRadius: 10,
+					elevation: 4,
+					shadowOffset: { width: 2, height: 2 },
+					shadowColor: 'rgba(0,0,0, 0.2)',
+					shadowOpacity: 0.5,
+					shadowRadius: 2,
+				}}
+				wrapperStyle={{
+					display: 'flex',
+					flexDirection: 'row',
+					justifyContent: 'space-between',
+					flexWrap: 'nowrap',
+					alignItems: 'center',
+				}}
+			>
+				<Card.Title style={{ marginBottom: 0 }}>
+					{' '}
+					{fav.name} {fav.price} €{' '}
+				</Card.Title>
+				<Button
+					type="clear"
+					onPress={() => {
+						handleFavDeletion(fav._id)
+					}}
+					icon={<Ionicons size={25} name="trash-outline" color="#FFC901" />}
+				/>
+			</Card>
+		)
+	})
 
+	async function handleFavDeletion(meal_id) {
+		var favFilter = favData.filter(e => e._id !== meal_id)
+		setFavData(favFilter)
+		var rawResponse = await fetch(
+			`http://${MY_IP}:3000/users/favorites/CnCEm57iQYtTb33A8kN4Evci8Sq_BOplZ/${meal_id}`,
+			{
+				method: 'DELETE',
+			}
+		)
+		var response = await rawResponse.json()
+		console.log(response)
+	}
 
-    var favList = favData.map((fav, i) => {
-        return (
-            <Card
-                key={i}
-                containerStyle={{
-                    borderRadius: 10,
-                    elevation: 4,
-                    shadowOffset: { width: 5, height: 5 },
-                    shadowColor: '#FFC901',
-                    shadowOpacity: 1,
-                    shadowRadius: 20,
-                }}
-                wrapperStyle={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    flexWrap: 'nowrap',
-                    alignItems: 'center',
-                }}
-            >
-                <Card.Title style={{ marginBottom: 0 }}>
-                    {' '}
-                    {fav.name} {fav.price} €{' '}
-                </Card.Title>
-                <Button
-                    type="clear"
-                    onPress={() => {
-                        handleFavDeletion(fav._id)
-                    }}
-                    icon={<Ionicons size={25} name="trash-outline" color="#FFC901" />}
-                />
-            </Card>
-        )
-    })
-
-    async function handleFavDeletion(meal_id) {
-        var favFilter = favData.filter(e => e._id !== meal_id)
-        setFavData(favFilter)
-        var rawResponse = await fetch(
-            `http://${MY_IP}:3000/users/favorites/CnCEm57iQYtTb33A8kN4Evci8Sq_BOplZ/${meal_id}`,
-            {
-                method: 'DELETE',
-            }
-        )
-        var response = await rawResponse.json()
-        console.log(response)
-    }
-
-    return (
-        <ScrollView>
-            <TopBar navigation={props.navigation} />
-            <Text
-                h3
-                style={{
-                    alignSelf: 'center',
-                    marginTop: 15,
-                    textDecorationLine: 'underline',
-                    color: '#FFC901',
-                }}
-            >
-                Favoris
-            </Text>
-            {favList}
-        </ScrollView>
-    )
+	return (
+		<ScrollView>
+			<TopBar navigation={props.navigation} />
+			<Text
+				h3
+				style={{
+					alignSelf: 'center',
+					marginTop: 15,
+					textDecorationLine: 'underline',
+					color: '#FFC901',
+				}}
+			>
+				Favoris
+			</Text>
+			{favList}
+		</ScrollView>
+	)
 }
 
 function mapStateToProps(state) {
-    return {
-        token: state.token,
-    }
+	return {
+		token: state.token,
+	}
 }
 export default connect(mapStateToProps, null)(Favorites)
