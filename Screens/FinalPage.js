d063d52545b66296c8609e561b210be898954c59
Marion Saul <marionsaul.dev@gmail.com>
2021-10-29 14:50:21 +0200

    fix name redirect final page

--- Screens/FinalPage.js
+++ Screens/FinalPage.js
@@ -6,44 +6,44 @@ import { connect } from 'react-redux'
 import Notification from '../Components/Notification'
 
 function FinalPage(props) {
-    return (
-        <View style={styles.container}>
-            <Image
-                source={require('../assets/VJF-logo.png')}
-                style={{ width: 200, height: 200, marginTop: 100 }}
-            />
+	return (
+		<View style={styles.container}>
+			<Image
+				source={require('../assets/VJF-logo.png')}
+				style={{ width: 200, height: 200, marginTop: 100 }}
+			/>
 
-            <Text h1 style={{ textAlign: 'center', color: '#000000', marginTop: 50 }}>
-                Vite j'ai faim!
-            </Text>
+			<Text h1 style={{ textAlign: 'center', color: '#000000', marginTop: 50 }}>
+				Vite j'ai faim!
+			</Text>
 
-            <Text h4 style={{ textAlign: 'center', color: '#F2A902', marginTop: 50 }}>
-                Merci pour votre confiance !
-            </Text>
-            <ScrollView style={{ marginTop: 50 }}>
-                <NextButton
-                    title="MOOD"
-                    onPress={() => {
-                        console.log('finish !!')
-                        Notification()
-                        props.navigation.navigate('Mood', { screen: 'Mood' })
-                    }}
-                />
-            </ScrollView>
-        </View>
-    )
+			<Text h4 style={{ textAlign: 'center', color: '#F2A902', marginTop: 50 }}>
+				Merci pour votre confiance !
+			</Text>
+			<ScrollView style={{ marginTop: 50 }}>
+				<NextButton
+					title="Commander encore"
+					onPress={() => {
+						console.log('finish !!')
+						Notification()
+						props.navigation.navigate('Mood', { screen: 'Mood' })
+					}}
+				/>
+			</ScrollView>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: '#F4F4F4',
-    },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		backgroundColor: '#F4F4F4',
+	},
 })
 //get token from store
 function mapStateToProps(state) {
-    return { token: state.token }
+	return { token: state.token }
 }
 
 export default connect(mapStateToProps, null)(FinalPage)
