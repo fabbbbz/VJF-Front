560740b602075f77d77d9fa015303159fbcfe9d3
Clementine <klmsenda@hotmail.com>
2021-11-01 12:09:50 +0100

    to push

--- reducers/address.js
+++ reducers/address.js
@@ -1,6 +1,6 @@
 export default function (address = '', action) {
 	if (action.type == 'STORE_ADDRESS') {
-		console.log('adding address to store')
+		// console.log('adding address to store')
 		return action.address
 	} else {
 		return address
