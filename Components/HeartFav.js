8c9a78aab776c73c73e7e2c4f013624f9c35254c
sebastien K <sebastienk21@gmail.com>
2021-11-02 15:55:54 +0100

    conflict res

--- Components/HeartFav.js
+++ Components/HeartFav.js
@@ -4,48 +4,6 @@ import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { connect } from 'react-redux'
 
 function HeartFav(props) {
-<<<<<<< HEAD
-
-    const [isFaved, setIsFaved] = useState(false)
-    var favList = []
-    const addToFavorite = async () => {
-        setIsFaved(true)
-        updateUser()
-
-    }
-
-    const updateUser = async () => {
-        try {
-            const token = props.token
-            const mealId = props.mealId
-
-            const data = await fetch(`https://vitejaifaim-master-i57witqbae0.herokuapp.com/users/favorites`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                body: `token=${token}&meal_id=${mealId}`,
-            })
-            const result = await data.json()
-
-
-
-        } catch (err) {
-            console.log(err.message)
-        }
-    }
-
-    if (isFaved == true) {
-        var heartPlusColor = { color: 'red' }
-    } else {
-        var heartPlusColor = { color: 'black' }
-    }
-
-    return (
-
-        <View >
-            <MaterialCommunityIcons style={heartPlusColor} name="heart-plus" size={24} onPress={() => addToFavorite('ajout fav')} />
-        </View >
-    )
-=======
 	const [isFaved, setIsFaved] = useState(false)
 
 	const addToFavorite = async () => {
@@ -86,7 +44,6 @@ function HeartFav(props) {
 			/>
 		</View>
 	)
->>>>>>> 864329c3231442d1f59a97935fdcc965f564659a
 }
 
 function mapStateToProps(state) {
