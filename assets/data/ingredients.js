030899456f014bec1ed7927ea11bac3224232409
Marion Saul <marionsaul.dev@gmail.com>
2021-11-03 13:21:28 +0100

    package json

---
+++ assets/data/ingredients.js
@@ -0,0 +1,7 @@
+const data = ['boeuf', 'poisson', 'poulet', 'crevettes', 'huitres']
+
+// const data = ingredients.map((ing, idx) => {
+// 	return { id: idx, title: ing }
+// })
+
+export default data
